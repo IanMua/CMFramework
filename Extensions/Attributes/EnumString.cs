50cca88f9fa0721bac1058d5737186862f69412b
Ian <darrenianmu@outlook.com>
2024-03-11 10:50:26 +0800

    添加特性拓展，添加了提取枚举特性值

---
+++ Extensions/Attributes/EnumString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace CMUFramework_Embark.Extensions.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EnumString : System.Attribute
+    {
+        public string value { get; }
+
+        public EnumString(string value)
+        {
+            this.value = value;
+        }
+    }
+    
+    public static class EnumStringAttributeExtensions
+    {
+        // Enum拓展方法
+        public static string GetEnumString(this System.Enum value)
+        {
+            Type type = value.GetType();
+            string name = System.Enum.GetName(type, value);
+            if (name == null) return null;
+
+            FieldInfo field = type.GetField(name);
+            if (field == null) return null;
+
+            EnumString attribute = 
+                (EnumString)System.Attribute.GetCustomAttribute(field, typeof(EnumString));
+            return attribute?.value;
+        }
+    }
+}
---
+++ Extensions/Attributes/EnumUShort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace CMUFramework_Embark.Extensions.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EnumUShort : System.Attribute
+    {
+        public ushort value { get; }
+
+        public EnumUShort(ushort value)
+        {
+            this.value = value;
+        }
+    }
+
+    public static class EnumUShortAttributeExtensions
+    {
+        // Enum拓展方法
+        public static ushort GetEnumUShort(this System.Enum value)
+        {
+            Type type = value.GetType();
+            string name = System.Enum.GetName(type, value);
+
+            FieldInfo field = type.GetField(name);
+
+            EnumUShort attribute =
+                (EnumUShort)System.Attribute.GetCustomAttribute(field, typeof(EnumUShort));
+            return attribute?.value ?? 0;
+        }
+    }
+}
