1d4d553a96ce32ac9e88c7b22e3882bd4d7636f2
Ian <darrenianmu@outlook.com>
2024-03-29 17:51:39 +0800

    添加架构的数据接口

---
+++ Architecture/IModel.cs
@@ -0,0 +1,31 @@
+﻿using CMUFramework_Embark.Architecture.Rule;
+
+namespace CMUFramework_Embark.Architecture
+{
+    public interface IModel : IBelongToArchitecture, ICanSetArchitecture, ICanGetUtility, ICanSendEvent
+    {
+        void Init();
+    }
+
+    public abstract class AbstractModel : IModel
+    {
+        private IArchitecture _architecture;
+
+        IArchitecture IBelongToArchitecture.GetArchitecture()
+        {
+            return _architecture;
+        }
+
+        void ICanSetArchitecture.SetArchitecture(IArchitecture architecture)
+        {
+            _architecture = architecture;
+        }
+
+        void IModel.Init()
+        {
+            OnInit();
+        }
+
+        protected abstract void OnInit();
+    }
+}
