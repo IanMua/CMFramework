c3f794c51f7901cbd82dcb6ea92d22d386071f92
Ian <darrenianmu@outlook.com>
2024-03-29 17:51:57 +0800

    添加架构的Controller接口

---
+++ Architecture/IController.cs
@@ -0,0 +1,9 @@
+﻿using CMUFramework_Embark.Architecture.Rule;
+
+namespace CMUFramework_Embark.Architecture
+{
+    public interface IController : IBelongToArchitecture, ICanSendCommand, ICanGetSystem, ICanGetModel,
+        ICanRegisterEvent
+    {
+    }
+}
