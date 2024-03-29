51a9658f1e7c9ef330b39331f7b2c907b2a9a8d7
Ian <darrenianmu@outlook.com>
2024-03-29 17:52:16 +0800

    修改架构的指令接口

--- Command/ICommand.cs
+++ Command/ICommand.cs
@@ -1,9 +1,41 @@
-﻿namespace CMUFramework_Embark.Command
+﻿using CMUFramework_Embark.Architecture;
+using CMUFramework_Embark.Architecture.Rule;
+
+namespace CMUFramework_Embark.Command
 {
-    public interface ICommand
+    public interface ICommand : IBelongToArchitecture, ICanSetArchitecture, ICanGetModel, ICanGetUtility, ICanGetSystem,
+        ICanSendCommand, ICanSendEvent
     {
         void Execute();
 
         void Undo();
+    }
+
+    public abstract class AbstractCommand : ICommand
+    {
+        private IArchitecture _architecture;
+
+        IArchitecture IBelongToArchitecture.GetArchitecture()
+        {
+            return _architecture;
+        }
+
+        void ICanSetArchitecture.SetArchitecture(IArchitecture architecture)
+        {
+            _architecture = architecture;
+        }
+
+        void ICommand.Execute()
+        {
+            OnExecute();
+        }
+
+        void ICommand.Undo()
+        {
+            OnUndo();
+        }
+
+        protected abstract void OnExecute();
+        protected abstract void OnUndo();
     }
 }
