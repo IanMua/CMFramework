a01b4579fbdbd4f66214b2ed3aa3ca2714e153e0
Ian <darrenianmu@outlook.com>
2024-03-29 17:50:14 +0800

    添加初始化架构的架构规则

---
+++ Architecture/ICanSetArchitecture.cs
@@ -0,0 +1,11 @@
+﻿namespace CMUFramework_Embark.Architecture
+{
+    public interface ICanSetArchitecture
+    {
+        /// <summary>
+        /// 给架构赋值
+        /// </summary>
+        /// <param name="architecture"></param>
+        void SetArchitecture(IArchitecture architecture);
+    }
+}
