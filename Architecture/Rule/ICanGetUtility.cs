17aa7f23efe277019064ba05b84a984bf2c6ecc5
Ian <darrenianmu@outlook.com>
2024-03-29 17:48:59 +0800

    添加获取工具的架构规则

---
+++ Architecture/Rule/ICanGetUtility.cs
@@ -0,0 +1,23 @@
+﻿namespace CMUFramework_Embark.Architecture.Rule
+{
+    /// <summary>
+    /// Architecture Rule 获取 Utility 接口
+    /// </summary>
+    public interface ICanGetUtility : IBelongToArchitecture
+    {
+    }
+
+    /// <summary>
+    /// Architecture Rule 获取 Utility 拓展方法
+    /// </summary>
+    public static class CanGetUtilityExtension
+    {
+        /// <summary>
+        /// 获取 Utility
+        /// </summary>
+        public static T GetUtility<T>(this ICanGetUtility self) where T : class, IUtility
+        {
+            return self.GetArchitecture().GetUtility<T>();
+        }
+    }
+}
